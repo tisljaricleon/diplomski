9d7bbfec0f52d0bcfa5a26e1923b9c5acae8babf
tisljaricleon <lt54120@fer.hr>
2026-03-19 17:42:44 +0100

    Fix errors

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+curl -X POST http://10.19.4.45:8080/fl/start \
+  -H "Content-Type: application/json" \
+  -d '{
+    "epochs": 10,
+    "localRounds": 5,
+    "trainingParams": {
+      "batchSize": 32,
+      "learningRate": 0.01
+    },
+    "modelSize": 123.0,
+    "costSource": "",
+    "costConfiguration": {},
+    "configurationModel": "minKld",
+    "rvaEnabled": false
+  }'
