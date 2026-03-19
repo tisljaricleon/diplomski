0fe2e9ed41f04897c8f59871798bd7cc1a9483e6
tisljaricleon <lt54120@fer.hr>
2026-03-19 17:06:51 +0100

    Pvc & Pv

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,5 +2,6 @@ cd ~/fl_orchestrator
 sudo bash scripts/cleanup.sh default
 cd ~
 git -C fl_orchestrator pull
+mkdir -p ~/fl_orchestrator/configs/cluster
 sudo cp /etc/rancher/k3s/k3s.yaml ~/fl_orchestrator/configs/cluster/kube_config.yaml
 sudo chmod 644 ~/fl_orchestrator/configs/cluster/kube_config.yaml
