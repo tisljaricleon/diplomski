d9f44bd8b3eeba580fae157db2ebc955b360ac7f
tisljaricleon <lt54120@fer.hr>
2026-06-02 03:50:12 +0200

    Experiments

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,7 @@ curl -X POST http://10.19.4.45:8080/fl/start \
       "learningRate": 0.001,
       "aomRoundsThreshold": 3,
       "aomSelectionEnabled": true,
-      "inflightThreshold": 9999999.0
+      "inflightThreshold": 150.0
     },
     "inferenceParams": {
       "enableServing": true
